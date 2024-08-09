b3af1aad5b6107514376db7b2cd9b6e6a2845105
Nikolas Kuhn <nikolaskuhn@gmx.de>
2024-08-09 22:19:05 +0200

    Change != to \neq

--- BeweiseMitComputer/EinfacheBeweise/Wurzeln.lean
+++ BeweiseMitComputer/EinfacheBeweise/Wurzeln.lean
@@ -14,21 +14,21 @@ example : ∀ r : ℚ, ∃ a b : ℤ,  r = (a /. b) := by
 example (r : ℚ) : (r.num).natAbs.Coprime r.den := by
   exact r.reduced
 
-theorem no_sqrt_2_in_rat : ∀ r : ℚ, r^2 != 2 := by
+theorem no_sqrt_2_in_rat : ∀ r : ℚ, r^2 ≠ 2 := by
   sorry
 
-theorem sqrt_2_irrational : ∀ r : ℚ, ((r : ℝ) != sqrt_2) := by
+theorem sqrt_2_irrational : ∀ r : ℚ, ((r : ℝ) ≠ sqrt_2) := by
   sorry
 
 -- Hier kann man `no_sqrt_2_in_rat` benutzen:
-theorem no_fourth_root_2_in_rat : ∀ r : ℚ, r^4 != 4 := by
+theorem no_fourth_root_2_in_rat : ∀ r : ℚ, r^4 ≠ 4 := by
   sorry
 
 -- Formuliere (und Beweise) das folgende Resultat selbst
 -- theorem no_rational_sqrt_5 : ... := by
 --  sorry
 
-theorem no_cube_root_2_in_rat : ∀ r : ℚ, r^3 != 2 := by
+theorem no_cube_root_2_in_rat : ∀ r : ℚ, r^3 ≠ 2 := by
   sorry
 
 theorem no_square_root_unless_perfect_square (r : ℚ) (n : ℕ):
